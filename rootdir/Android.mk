95b15fbdf4837ef3f508bd5522ec5f2d7363d3e5
Vladimir Oltean <olteanv@gmail.com>
2017-08-09 02:35:34 +0300

    rootdir: remove init.class_main.sh prebuilt package
    Change-Id: I3b855afcb726846fad0b7ee5bc97de76b4c8fc8c
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,10 +2,3 @@ LOCAL_PATH := $(call my-dir)
 
 # YTX703L specific configuration scripts
 
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.class_main.sh
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.class_main.sh
-include $(BUILD_PREBUILT)
-
